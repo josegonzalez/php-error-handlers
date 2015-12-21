40bd11a9852af7b1da727507509219219b9194bf
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-12-20 23:04:44 -0500

    Remove TODO

---
+++ example.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+
+// Create an array of configuration data to pass to the handler class
+$config = [
+    'handlers' => [
+        'MonologStreamHandler' => [
+        ],
+        // *Can* be the class name, not-namespaced
+        'NewrelicHandler' => [
+        ],
+        // Can also include the full namespace
+        'Josegonzalez\ErrorHandlers\Handler\BugsnagHandler' => [
+            'apiKey' => 'YOUR_API_KEY_HERE'
+        ],
+    ],
+];
+
+// Register the error handler
+(new \Josegonzalez\ErrorHandlers\Error\Handler($config))->register();
+
+// Enjoy throwing exceptions and reporting them upstream
+throw new \Exception('Test Exception');
